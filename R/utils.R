e8ad820ec2ee1e63fbbbbfc76f209ad86bf101f3
Dean Attali <daattali@gmail.com>
2015-07-31 01:52:22 -0700

    remove helpers.R file and move its contents to utils.R

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,43 @@
 ## ddpcr - R package for analysis of droplet digital PCR data
 ## Copyright (C) 2015 Dean Attali
 
+# Retrieve droplet data for a single well.
+#
+# Args:
+#   .wellData: The dataframe containing all the droplets
+#   .well: The id of the well of interest
+#   .full: If true, get all droplets, including the empty ones
+#   .clusters: If true, return information about which cluster each drop belongs to
+#
+# Returns:
+#   Dataframe containing only droplet data for the given well
+get_single_well <- function(plate, well_id,
+                            empty = FALSE, outliers = FALSE, clusters = FALSE) {
+  stopifnot(plate %>% inherits("ddpcr_plate"))
+  
+  well_id %<>% toupper
+  
+  result <-
+    plate_data(plate) %>%
+    dplyr::filter_(~ well == well_id) %>%
+    dplyr::select_(quote(-well))
+  
+  if (!empty) {
+    result %<>%
+      dplyr::filter_(~ cluster != plate %>% cluster('EMPTY'))
+  }
+  if (!outliers) {
+    result %<>%
+      dplyr::filter_(~ cluster != plate %>% cluster('OUTLIER'))
+  }
+  if (!clusters) {
+    result %<>%
+      dplyr::select_(~ -cluster)
+  }
+  
+  result
+}
+
 #' Show an error message
 #' @param x The error message text
 #' @keywords internal
