048c554a5c2a385ee14ddf1122378317f3f88dde
Dean Attali <daattali@gmail.com>
2017-05-19 01:25:21 -0400

    work with dplyr > 0.5.0

--- R/empty.R
+++ R/empty.R
@@ -2,23 +2,23 @@
 ## Copyright (C) 2015 Dean Attali
 
 #' Analysis step: Remove empty droplets
-#' 
+#'
 #' Find the empty droplets (double-negative droplets) in each well in a plate
 #' and assign these droplets to the \emph{EMPTY} cluster. \cr\cr
 #' \href{https://github.com/daattali/ddpcr#advanced-topic-2-algorithms-used-in-each-step}{See the README} for
 #' more information about the algorithm used to find empty droplets.
-#' 
+#'
 #' This function is recommended to be run as part of an analysis pipeline (ie.
 #' within the \code{\link[ddpcr]{analyze}} function) rather than being called
 #' directly.
-#' 
+#'
 #' @param plate A ddPCR plate.
 #' @return A ddPCR plate with the empty droplets marked as empty. The plate's
 #' metadata will have a few new variables relating to the empty droplets.
 #' @seealso \code{\link[ddpcr]{analyze}}\cr
 #' \code{\link[ddpcr]{get_empty_cutoff}}
 #' @note This is an S3 generic, which means that different ddPCR plate types can
-#' implement this function differently. 
+#' implement this function differently.
 #' \href{https://github.com/daattali/ddpcr#advanced-topic-3-creating-new-plate-types}{See the README} for
 #' more information on how to implement custom ddPCR plate types.
 #' @export
@@ -35,11 +35,11 @@ remove_empty.ddpcr_plate <- function(plate) {
   CURRENT_STEP <- plate %>% step('REMOVE_EMPTY')
   plate %>% check_step(CURRENT_STEP)
   step_begin("Identifying empty droplets")
-  
+
   # ---
 
   if (length(wells_success(plate)) > 0) {
-    
+
     # get the empty cutoff of every well
     empty_cutoff_map <-
       vapply(
@@ -48,38 +48,41 @@ remove_empty.ddpcr_plate <- function(plate) {
         list("x", "y")
       ) %>%
       lol_to_df
-    
+
     # set the cluster to EMPTY for every empty droplet in every well
     x_var <- x_var(plate)
     y_var <- y_var(plate)
     CLUSTERS_UNANALYZED <- unanalyzed_clusters(plate, 'EMPTY')
+
+    do_work <- function(well_data) {
+      well_id = well_data[['well']][1]
+
+      if (!well_id %in% empty_cutoff_map[['well']]) {
+        return(well_data)
+      }
+
+      # find the cutoffs for this well and mark all droplets below as empty
+      cutoffs <- empty_cutoff_map %>% dplyr::filter_(~ well == well_id)
+      cutoff_x <- cutoffs[['x']]
+      cutoff_y <- cutoffs[['y']]
+
+      empty_idx <- well_data[['cluster']] %in% CLUSTERS_UNANALYZED
+      if (!is.na(cutoff_x)) {
+        empty_idx <- empty_idx & well_data[[x_var]] < cutoff_x
+      }
+      if (!is.na(cutoff_y)) {
+        empty_idx <- empty_idx & well_data[[y_var]] < cutoff_y
+      }
+      well_data[empty_idx, 'cluster'] <- plate %>% cluster('EMPTY')
+      well_data
+    }
+
     data <-
       plate_data(plate) %>%
       dplyr::group_by_("well") %>%
-      dplyr::do({
-        well_data <- .
-        well_id = well_data[['well']][1]
-        if (!well_id %in% empty_cutoff_map[['well']]) {
-          return(well_data)
-        }
-        
-        # find the cutoffs for this well and mark all droplets below as empty
-        cutoffs <- empty_cutoff_map %>% dplyr::filter_(~ well == well_id)
-        cutoff_x <- cutoffs[['x']]
-        cutoff_y <- cutoffs[['y']]
-        
-        empty_idx <- well_data[['cluster']] %in% CLUSTERS_UNANALYZED
-        if (!is.na(cutoff_x)) {
-          empty_idx <- empty_idx & well_data[[x_var]] < cutoff_x
-        }
-        if (!is.na(cutoff_y)) {
-          empty_idx <- empty_idx & well_data[[y_var]] < cutoff_y
-        }
-        well_data[empty_idx, 'cluster'] <- plate %>% cluster('EMPTY')
-        well_data
-      }) %>%
+      dplyr::do(do_work(.)) %>%
       dplyr::ungroup()
-    
+
     # calculate a few metadata variables
     meta <-
       data %>%
@@ -96,25 +99,25 @@ remove_empty.ddpcr_plate <- function(plate) {
         ),
         c("drops_non_empty", "drops_empty_fraction")
       ))
-    
+
     # ---
-    
+
     # set the new drop data and metadata into the plate
     plate_data(plate) <- data
     plate_meta(plate) <- meta
-    
+
     # now that we have information about empty droplets, calculate template concentrations
     plate %<>% calculate_concentration
   }
-  
+
   status(plate) <- CURRENT_STEP
   step_end()
-  
+
   plate
 }
 
 #' Get the cutoff for empty droplets in a well
-#' 
+#'
 #' Calculate the cutoff thresholds that define which droplets are empty in a well.
 #' @return A list with 2 elements named \code{x} and \code{y} with the values
 #' being the cutoff in the corresponding axis.
@@ -129,13 +132,13 @@ get_empty_cutoff <- function(plate, well_id) {
 #' @keywords internal
 get_empty_cutoff.ddpcr_plate <- function(plate, well_id) {
   well_data <- get_single_well(plate, well_id, empty = TRUE)
-  
+
   # fit two normal distributions in the data along the Y dimension
   set.seed(params(plate, 'GENERAL', 'RANDOM_SEED'))
   quiet(
     mixmdl_y <- mixtools::normalmixEM(well_data[[y_var(plate)]], k = 2)
   )
-  
+
   # set the Y cutoff as the mean (mu) of the 1st component + k standard deviations
   smaller_comp_y <- mixmdl_y$mu %>% which.min
   cutoff_y <-
@@ -144,7 +147,7 @@ get_empty_cutoff.ddpcr_plate <- function(plate, well_id) {
     ) %>%
     ceiling %>%
     as.integer
-  
+
   # repeat the above along the X dimension
   set.seed(params(plate, 'GENERAL', 'RANDOM_SEED'))
   quiet(
@@ -156,7 +159,7 @@ get_empty_cutoff.ddpcr_plate <- function(plate, well_id) {
         params(plate, 'REMOVE_EMPTY', 'CUTOFF_SD') * mixmdl_x$sigma[smaller_comp_x]
     ) %>%
     ceiling %>%
-    as.integer  
-  
-  return(list("x" = cutoff_x, "y" = cutoff_y)) 
+    as.integer
+
+  return(list("x" = cutoff_x, "y" = cutoff_y))
 }
--- R/pnpp_experiment-classify.R
+++ R/pnpp_experiment-classify.R
@@ -2,18 +2,18 @@
 ## Copyright (C) 2015 Dean Attali
 
 #' Analysis step: Classify droplets
-#' 
+#'
 #' The main analysis step for ddPCR plates of type \code{pnpp_experiment}.
 #' Classify each droplet as either rain, ++, or +-. Also calculate the frequency
 #' of negative droplets, and attempt to detemine if each well has a statistically
 #' significant number of such droplets.\cr\cr
 #' \href{https://github.com/daattali/ddpcr#advanced-topic-2-algorithms-used-in-each-step}{See the README} for
 #' more information about the algorithm used.
-#' 
+#'
 #' This function is recommended to be run as part of an analysis pipeline (ie.
 #' within the \code{\link[ddpcr]{analyze}} function) rather than being called
 #' directly.
-#' 
+#'
 #' @param plate A ddPCR plate.
 #' @return A ddPCR plate with all droplets assigned to a cluster. The plate's
 #' metadata will have several new variables.
@@ -21,9 +21,9 @@
 #' \code{\link[ddpcr]{classify_droplets_single}}\cr
 #' \code{\link[ddpcr]{mark_clusters}}\cr
 #' \code{\link[ddpcr]{has_signif_negative_cluster}}
-#' 
+#'
 #' @note This is an S3 generic, which means that different ddPCR plate types can
-#' implement this function differently. 
+#' implement this function differently.
 #' \href{https://github.com/daattali/ddpcr#advanced-topic-3-creating-new-plate-types}{See the README} for
 #' more information on how to implement custom ddPCR plate types.
 #' @export
@@ -38,13 +38,13 @@ classify_droplets <- function(plate) {
 #' @keywords internal
 classify_droplets.pnpp_experiment <- function(plate) {
   CURRENT_STEP <- plate %>% step('CLASSIFY')
-  plate %>% check_step(CURRENT_STEP)  
+  plate %>% check_step(CURRENT_STEP)
   step_begin("Classifying droplets")
-  
+
   # ---
- 
+
   if (length(wells_success(plate)) > 0) {
-     
+
     # get droplet classifications in each well
     well_clusters_info <-
       vapply(wells_success(plate),
@@ -52,44 +52,44 @@ classify_droplets.pnpp_experiment <- function(plate) {
              vector(mode = "list", length = 3)) %>%
       lol_to_df %>%
       magrittr::set_names(lapply(names(.), function(x) meta_var_name(plate, x)))
-    
+
     # add metadata to each well
     plate_meta(plate) %<>%
       merge_dfs_overwrite_col(well_clusters_info)
-    
+
     # mark the droplets with their assigned cluster in the plate and calculate frequencies
     plate %<>%
       mark_clusters(plate %>% wells_success) %>%
       calculate_negative_freqs
   }
-  
+
   # ---
-  
+
   status(plate) <- CURRENT_STEP
   step_end()
-  
+
   plate
 }
 
-#' Classify droplets in a well 
-#' 
+#' Classify droplets in a well
+#'
 #' This function runs the actual algorithm for classifying droplets in a single
-#' well. 
+#' well.
 #' @keywords internal
 #' @export
 classify_droplets_single <- function(plate, well_id, ...) {
   UseMethod("classify_droplets_single")
 }
 
-#' Classify droplets in a well 
-#' 
+#' Classify droplets in a well
+#'
 #' If you want to see details about how a well was classified, you can set
 #' \code{plot = TRUE} to plot the results.
 #' @export
 #' @keywords internal
 classify_droplets_single.pnpp_experiment <- function(plate, well_id, ..., plot = FALSE) {
   stopifnot(plate %>% inherits("pnpp_experiment"))
-  
+
   signif_negative_cluster <- FALSE
   well_data <- get_single_well(plate, well_id)
   variable_var <- variable_dim_var(plate)
@@ -97,14 +97,14 @@ classify_droplets_single.pnpp_experiment <- function(plate, well_id, ..., plot =
   # get the filled borders (threshold under which all droplets are considered rain)
   filled_border <- get_filled_border(plate, well_id)
   filled <- get_filled_drops(plate, well_id, filled_border)
-  
+
   # whether or not the result is found using the first attempted bandwidth
   bw_orig <- TRUE
   # previous "adjust" value
-  adj_prev <- NULL 
+  adj_prev <- NULL
   # final "adjust" value
-  adj_final <- NULL 
-  
+  adj_final <- NULL
+
   # try to find the optimal bandwidth for the density kernel
   for (adj in seq(params(plate, 'CLASSIFY', 'ADJUST_BW_MIN'),
                   params(plate, 'CLASSIFY', 'ADJUST_BW_MAX'),
@@ -112,7 +112,7 @@ classify_droplets_single.pnpp_experiment <- function(plate, well_id, ..., plot =
     dens_smooth <- stats::density(filled[[variable_var]], bw = "sj", adjust = adj)
     maxima_idx <- local_maxima(dens_smooth$y)
     minima_idx <- local_minima(dens_smooth$y)
-    
+
     # ensure the right peak is centered on the wildype cluster and not a few
     # outlier drops
     while (TRUE) {
@@ -120,7 +120,7 @@ classify_droplets_single.pnpp_experiment <- function(plate, well_id, ..., plot =
         filled %>%
         dplyr::filter_(lazyeval::interp(~ var %btwn% dens_smooth$x[utils::tail(minima_idx, 2)],
                                         var = as.name(variable_var)))
-      
+
       if (nrow(btwn_right_mins) < nrow(filled) * 0.1) {
         # discard rightmost extreme points
         maxima_idx %<>% utils::head(-1)
@@ -129,10 +129,10 @@ classify_droplets_single.pnpp_experiment <- function(plate, well_id, ..., plot =
         break
       }
     }
-    
+
     # number of peaks = number of local maxima
     num_peaks <- length(maxima_idx)
-    
+
     # if there is only one peak, record the bandwidth and stop
     if (num_peaks == 1) {
       # if it's not the first attempt, revert to the previous bandwidth
@@ -151,16 +151,16 @@ classify_droplets_single.pnpp_experiment <- function(plate, well_id, ..., plot =
       adj_final <- adj
       break
     }
-    
+
     # if there are more than 2 peaks, increase the bandwidth
     adj_prev <- adj
     bw_orig <- FALSE
   }
   if (is.null(adj_final)) {
-    warn_msg(paste0("Could not accurately analyze well ", well_id, 
+    warn_msg(paste0("Could not accurately analyze well ", well_id,
                     ", please double-check the results to ensure it looks ok"))
   }
-  
+
   # if there is only one peak, then everything is positive
   # otherwise, use the local minimum to the right of the left-most peak
   # usually there will only be two peaks, so this means the only local minimum.
@@ -172,15 +172,15 @@ classify_droplets_single.pnpp_experiment <- function(plate, well_id, ..., plot =
     minimas <- dens_smooth$x[minima_idx]
     negative_border <- minimas[which(minimas > left_peak) %>% min]
   }
-  
+
   # mark all the negative and positive droplets according to the border
   negative_drops <- filled %>%
     dplyr::filter_(lazyeval::interp(~ var <= negative_border,
-                                    var = as.name(variable_var)))    
+                                    var = as.name(variable_var)))
   positive_drops <- filled %>%
     dplyr::filter_(lazyeval::interp(~ var > negative_border,
-                                    var = as.name(variable_var)))   
-  
+                                    var = as.name(variable_var)))
+
   # to try to replicate the human approach, we want to get the mutant border as
   # close as possible to the mutant cluster rather than in the middle between
   # mutants and wildtypes
@@ -191,14 +191,14 @@ classify_droplets_single.pnpp_experiment <- function(plate, well_id, ..., plot =
     negative_border <- negative_border_tight
     negative_drops <- filled %>%
       dplyr::filter_(lazyeval::interp(~ var <= negative_border,
-                                      var = as.name(variable_var)))    
+                                      var = as.name(variable_var)))
     positive_drops <- filled %>%
       dplyr::filter_(lazyeval::interp(~ var > negative_border,
-                                      var = as.name(variable_var))) 
-  }  
-  
+                                      var = as.name(variable_var)))
+  }
+
   negative_freq <- calc_negative_freq_simple(nrow(negative_drops), nrow(positive_drops))
-  
+
   if (plot) {
     graphics::plot(
       well_data,
@@ -214,11 +214,11 @@ classify_droplets_single.pnpp_experiment <- function(plate, well_id, ..., plot =
     graphics::abline(v = dens_smooth$x[maxima_idx], col = "grey")
     graphics::abline(v = negative_border_tight, col = "red")
   }
-  
-  # detemine if the well has a statistically significant negative cluster 
+
+  # detemine if the well has a statistically significant negative cluster
   signif_negative_cluster <-
     has_signif_negative_cluster(plate, nrow(negative_drops), nrow(positive_drops))
-  
+
   return(list(
     'negative_border'  = as.integer(negative_border),
     'filled_border'    = filled_border,
@@ -227,7 +227,7 @@ classify_droplets_single.pnpp_experiment <- function(plate, well_id, ..., plot =
 }
 
 #' Mark the clusters of droplets only in certain wells to their assigned cluster
-#' 
+#'
 #' This function simply looks at all droplets of certain wells and marks the
 #' cluster of each droplet according to the gates that are already calculated.
 #' This function is called once after \code{\link[ddpcr]{classify_droplets_single}}
@@ -238,46 +238,46 @@ classify_droplets_single.pnpp_experiment <- function(plate, well_id, ..., plot =
 mark_clusters <- function(plate, wells) {
   positive_var <- positive_dim_var(plate)
   variable_var <- variable_dim_var(plate)
-  
+
   CLUSTER_RAIN <- plate %>% cluster('RAIN')
   CLUSTER_POSITIVE <- plate %>% cluster('POSITIVE')
   CLUSTER_NEGATIVE <- plate %>% cluster('NEGATIVE')
   CLUSTERS_UNANALYZED <- unanalyzed_clusters(plate, 'RAIN')
-  
+
   # for every well in the list of wells we're interested in classifying,
   # get the filled border and the negative border, and based on that classify
   # all droplets as rain, positive, or negative
+  do_work <- function(well_data) {
+    well_id = well_data[['well']][1]
+    if (!well_id %in% wells) {
+      return(well_data)
+    }
+
+    filled_border <- well_info(plate, well_id, 'filled_border')
+    negative_border <- well_info(plate, well_id, meta_var_name(plate, 'negative_border'))
+
+    classifiable_idx <- well_data[['cluster']] %in% CLUSTERS_UNANALYZED
+    filled_idx <- classifiable_idx & well_data[[positive_var]] >= filled_border
+    negative_idx <- filled_idx & well_data[[variable_var]] <= negative_border
+    positive_idx <- filled_idx & well_data[[variable_var]] > negative_border
+
+    well_data[classifiable_idx, 'cluster'] <- CLUSTER_RAIN
+    well_data[negative_idx, 'cluster'] <- CLUSTER_NEGATIVE
+    well_data[positive_idx, 'cluster'] <- CLUSTER_POSITIVE
+    well_data
+  }
   data <-
     plate_data(plate) %>%
     dplyr::group_by_("well") %>%
-    dplyr::do({
-      well_data <- .
-      well_id = well_data[['well']][1]
-      if (!well_id %in% wells) {
-        return(well_data)
-      }
-      
-      filled_border <- well_info(plate, well_id, 'filled_border')
-      negative_border <- well_info(plate, well_id, meta_var_name(plate, 'negative_border'))
-      
-      classifiable_idx <- well_data[['cluster']] %in% CLUSTERS_UNANALYZED
-      filled_idx <- classifiable_idx & well_data[[positive_var]] >= filled_border
-      negative_idx <- filled_idx & well_data[[variable_var]] <= negative_border
-      positive_idx <- filled_idx & well_data[[variable_var]] > negative_border
-      
-      well_data[classifiable_idx, 'cluster'] <- CLUSTER_RAIN
-      well_data[negative_idx, 'cluster'] <- CLUSTER_NEGATIVE
-      well_data[positive_idx, 'cluster'] <- CLUSTER_POSITIVE
-      well_data
-    }) %>%
+    dplyr::do(do_work(.)) %>%
     dplyr::ungroup()
-  
+
   plate_data(plate) <- data
   plate
 }
 
 #' Does a well have a statistically significant number of negative droplets?
-#' 
+#'
 #' Classify a well as having a significant negative cluster (eg. a mutant well)
 #' or not using a binomial test.\cr\cr
 #' We can call a well as mutant if it is statistically significantly more than
@@ -305,8 +305,8 @@ has_signif_negative_cluster <- function(plate, neg, pos) {
   freq_threshold <- params(plate, 'CLASSIFY', 'SIGNIFICANT_NEGATIVE_FREQ')
   pval <- params(plate, 'CLASSIFY', 'SIGNIFICANT_P_VALUE')
   total <- neg + pos
-  result <- 
-    (1 - stats::pbinom(neg, total, freq_threshold) + 
+  result <-
+    (1 - stats::pbinom(neg, total, freq_threshold) +
        stats::dbinom(neg, total, freq_threshold)) < pval
   result
 }
